d9dee8b2b88a7f645d6cc8aae45544b9f07c6953
Jared Grippe <jared@deadlyicon.com>
2009-01-03 22:48:52 -0800

    added tmp dir ignore and commented out flush command

--- specs/spec_helper.rb
+++ specs/spec_helper.rb
@@ -6,7 +6,7 @@ require 'spec'
 
 TEMP_DIR   = File.join(File.dirname(__FILE__), 'tmp')
 
-def flush_tmp_dir
-  Dir["#{TMP_DIR}/*"].each{|f| FileUtils.rm_rf(f) }
-end
+# def flush_tmp_dir
+#   Dir["#{TMP_DIR}/*"].each{|f| FileUtils.rm_rf(f) }
+# end
 
